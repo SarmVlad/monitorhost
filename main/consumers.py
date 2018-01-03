9cd5cc94712863e0a9869b2edda577211f73c127
Vladisval <33662554+Vladisval@users.noreply.github.com>
2018-01-04 02:35:54 +0300

    обновка

--- main/consumers.py
+++ main/consumers.py
@@ -17,26 +17,27 @@ def http_request_consumer(message):
 
 
 @channel_session_user_from_http
-def ws_connect(message):
+def ws_connect(message, chat_id = 9 ):
+
+    print(chat_id)
     # Accept the incoming connection
     message.reply_channel.send({"accept": True})
     # Add them to the chat group
-    Group("chat").add(message.reply_channel)
-    print("connect")
+    Group("chat-%s" % chat_id).add(message.reply_channel)
+    print('connected')
 
 
 @channel_session_user
-def ws_message(message):
-    mess = json.loads(message.content['text'])
+def ws_message(message, chat_id):
     user = models.User.objects.get(username=message.user.username)
 
-    models.Chat.objects.get(id=mess['chat_id']).message_set.create(author=user, text=mess['text'])
-    Group("chat").send({
-        "text": '{"username" : " ' +user.username+ ' ", "date" : " ' +timezone.now().__str__()+' ", "text" : " ' +mess['text']+ ' "}',
+    models.Chat.objects.get(id=chat_id).message_set.create(author=user, text=message['text'])
+    Group("chat-%s" % chat_id).send({
+        "text": '{"username" : " ' +user.username+ ' ", "date" : " ' +timezone.now().__str__()+' ", "text" : " ' +message['text']+ ' "}',
     })
 
 
 @channel_session_user
-def ws_disconnect(message):
-    Group('chat').discard(message.reply_channel)
+def ws_disconnect(message, chat_id):
+    Group("chat-%s" % chat_id).discard(message.reply_channel)
     print("disconnect")
--- monitorhost/routing.py
+++ monitorhost/routing.py
@@ -3,7 +3,7 @@ from channels.routing import route
 from main import consumers
 
 channel_routing = [
-    route("websocket.connect", consumers.ws_connect),
-    route("websocket.receive", consumers.ws_message),
-    route("websocket.disconnect", consumers.ws_disconnect),
+    route("websocket.connect", consumers.ws_connect, path=r"^/chat/(?P<chat_id>[0-9]+)/$"),
+    route("websocket.receive", consumers.ws_message, path=r"^/chat/(?P<chat_id>[0-9]+)/$"),
+    route("websocket.disconnect", consumers.ws_disconnect, path=r"^/chat/(?P<chat_id>[0-9]+)/$"),
 ]
